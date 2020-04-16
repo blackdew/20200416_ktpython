547b9738e3afa30f1cc7e91586e16a5fbc282faf
sookbun <blackdew7@gmail.com>
2020-04-16 16:45:45 +0900

    cgi-bin

--- my_first.py
+++ my_first.py
@@ -29,11 +29,11 @@ print("메일 내용입니다.")
 print("*" * 50)
 print(complete_message)
 
-f = open('./mail.txt', 'w')
-f.write(complete_message)
-f.close()
+# f = open('./mail.txt', 'w')
+# f.write(complete_message)
+# f.close()
 
-with open('./mail.txt', 'w') as f:
+with open('./mail.txt', 'w', encoding="utf-8") as f:
     f.write(complete_message)
 
 # 메일을 바로 발송
