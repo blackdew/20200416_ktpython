f16b722479034366f4caad02c4afbee6ff5517a3
sookbun <blackdew7@gmail.com>
2020-04-16 10:45:05 +0900

    반복문과 조건문

--- my_first.py
+++ my_first.py
@@ -5,15 +5,25 @@ message = """안녕하세요, {0}님
 남은 기간 즐겁고 행복한 공부가 되길 기원합니다.
 """
 
-# 누구에게 보낼지 이름을 입력을 받아서
-who = input("누구에게 보낼건가요? ")
-date = input("오늘은 몇일째 수업인가요? ")
+while(True):
+    # 누구에게 보낼지 이름을 입력을 받아서
+    who = input("누구에게 보낼건가요? ")
+    date = input("오늘은 몇일째 수업인가요? ")
 
-# 메일 내용을 완성
-complete_message = message.format(who, date)
+    # 메일 내용을 완성
+    complete_message = message.format(who, date)
 
-# 출력한다.
-print(complete_message)
+    # 출력한다.
+    print()
+    print("*" * 50)
+    print("메일 내용입니다.")
+    print("*" * 50)
+    print(complete_message)
+    
+    end = input("끝낼까요? (y/n) ")
+    if (end == 'y'):
+        print("종료합니다.")
+        break
 
 # 메일을 바로 발송
 # send_mail(complete_message)
