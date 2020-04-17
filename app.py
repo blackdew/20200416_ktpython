ec98a74518d6e5a27d76fd6a1f9c071a5a07b0b1
sookbun <blackdew7@gmail.com>
2020-04-17 09:27:32 +0900

    add more route

--- app.py
+++ app.py
@@ -9,3 +9,27 @@ def index():
         html = f.read()
     
     return html
+
+@app.route("/html")
+def html():
+    # views/index.html 에서 파일을 받아오자 
+    with open('views/1.html', 'r') as f:
+        html = f.read()
+    
+    return html
+
+@app.route("/CSS")
+def css():
+    # views/index.html 에서 파일을 받아오자 
+    with open('views/2.html', 'r') as f:
+        html = f.read()
+    
+    return html
+
+@app.route("/Javascript")
+def javascript():
+    # views/index.html 에서 파일을 받아오자 
+    with open('views/3.html', 'r') as f:
+        html = f.read()
+    
+    return html
