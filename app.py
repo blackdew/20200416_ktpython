3d73a944fc54a6dff62cce11c55a78a5a0f73b97
sookbun <blackdew7@gmail.com>
2020-04-17 13:42:32 +0900

    패스워드 확인

--- app.py
+++ app.py
@@ -39,13 +39,17 @@ def login():
     template = get_template('login.html')
     
     if request.method == 'GET':
-        return template
+        return template.format("")
+    
     elif request.method == 'POST':
         # 만약 회원이 아니면, "회원이 아닙니다."라고 알려주자
         m = [e for e in members if e['id'] == request.form['id']]
         if len(m) == 0:
-            return template + "회원이 아닙니다."
-            
+            return template.format("<p>회원이 아닙니다.</p>")
         
+        # 만약 패스워드가 다르면, "패스워드를 확인해 주세요"라고 알려주자
+        if request.form['pw'] != m[0]['pw']:
+            return template.format("<p>패스워드를 확인해 주세요</p>")
+            
         # 로그인 성공에는 메인으로
         return redirect("/")
