5f82fe01d8433104740a6c2fde8897297a9692b1
sookbun <blackdew7@gmail.com>
2020-04-17 10:22:29 +0900

    add template function

--- app.py
+++ app.py
@@ -2,30 +2,29 @@ from flask import Flask
 
 app = Flask("flask")
 
+def get_template(filename):
+    with open('views/'+filename, 'r', encoding="utf-8") as f:
+        template = f.read()
+        
+    return template
+    
+
 @app.route("/")
 def index():
-    with open('views/template.html', 'r', encoding="utf-8") as f:
-        template = f.read()
-    
+    template = get_template('template.html')
     return template.format('Welcome', 'Welcome Python Class...')
 
 @app.route("/html")
 def html():
-    with open('views/template.html', 'r', encoding="utf-8") as f:
-        template = f.read()
-    
+    template = get_template('template.html')
     return template.format('HTML', 'HTML is...')
 
 @app.route("/CSS")
 def css():
-    with open('views/template.html', 'r', encoding="utf-8") as f:
-        template = f.read()
-    
+    template = get_template('template.html')
     return template.format('CSS', 'CSS is...')
 
 @app.route("/Javascript")
 def javascript():
-    with open('views/template.html', 'r', encoding="utf-8") as f:
-        template = f.read()
-    
+    template = get_template('template.html')
     return template.format('Javascript', 'Javascript is...')
