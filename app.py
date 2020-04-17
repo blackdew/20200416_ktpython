ba6da2d2907640f7c3e66b69083e117b205c7b98
sookbun <blackdew7@gmail.com>
2020-04-17 10:06:24 +0900

    use template.html

--- app.py
+++ app.py
@@ -4,28 +4,28 @@ app = Flask("flask")
 
 @app.route("/")
 def index():
-    with open('views/index.html', 'r', encoding="utf-8") as f:
-        html = f.read()
+    with open('views/template.html', 'r', encoding="utf-8") as f:
+        template = f.read()
     
-    return html
+    return template.format('Welcome', 'Welcome Python Class...')
 
 @app.route("/html")
 def html():
-    with open('views/1.html', 'r', encoding="utf-8") as f:
-        html = f.read()
+    with open('views/template.html', 'r', encoding="utf-8") as f:
+        template = f.read()
     
-    return html
+    return template.format('HTML', 'HTML is...')
 
 @app.route("/CSS")
 def css():
-    with open('views/2.html', 'r', encoding="utf-8") as f:
-        html = f.read()
+    with open('views/template.html', 'r', encoding="utf-8") as f:
+        template = f.read()
     
-    return html
+    return template.format('CSS', 'CSS is...')
 
 @app.route("/Javascript")
 def javascript():
-    with open('views/3.html', 'r', encoding="utf-8") as f:
-        html = f.read()
+    with open('views/template.html', 'r', encoding="utf-8") as f:
+        template = f.read()
     
-    return html
+    return template.format('Javascript', 'Javascript is...')
