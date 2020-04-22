338c27fa8784724c720145c22833d3447aaed235
sookbun <blackdew7@gmail.com>
2020-04-22 09:23:16 +0900

    use template

--- day7/mysite.py
+++ day7/mysite.py
@@ -1,6 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates")
 
 # 자동갱신 되도록 설정
 app.config['ENV'] = 'development'
@@ -9,6 +9,10 @@ app.config['DEBUG'] = True
 @app.route("/")
 def index():
     return "welcome day7 class 2222"
+
+@app.route("/exam01")
+def exam01():
+    return render_template('base.html')
 
 # python 파일명으로 실행을 위해서 필요
 app.run(port=8001)
