2368d51d7930120ed3e90ec57edfc9ecc96fab4a
sookbun <blackdew7@gmail.com>
2020-04-22 09:27:13 +0900

    show result in template

--- day7/mysite.py
+++ day7/mysite.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__, template_folder="templates")
 
@@ -10,9 +10,13 @@ app.config['DEBUG'] = True
 def index():
     return "welcome day7 class 2222"
 
-@app.route("/exam01")
+@app.route("/exam01", methods=['get', 'post'])
 def exam01():
-    return render_template('base.html')
+    if request.method == 'POST':
+        result = request.form['numbers']
+        
+    return render_template('base.html', 
+                           result="어쩌고저쩌고")
 
 # python 파일명으로 실행을 위해서 필요
 app.run(port=8001)
