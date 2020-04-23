b778c6985a8c4d9a2071d9c934e8519f3a595772
sookbun <blackdew7@gmail.com>
2020-04-23 09:24:13 +0900

    add template

--- day8/day8.py
+++ day8/day8.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__, template_folder="templates")
 app.config['ENV'] = 'development'
@@ -7,5 +7,16 @@ app.config['DEBUG'] = True
 @app.route('/')
 def index():
     return "welcome, day8 class"
+
+@app.route('/verify_jumin', methods=['get', 'method'])
+def verify_jumin():
+    result = ''
+    
+    return render_template('template.html',
+                            title='주민등록번호 검증기',
+                            action='verify_jumin',
+                            var_name='jumin',
+                            placeholder='000000-0000000',
+                            result=result)
 
 app.run(port=8000)
